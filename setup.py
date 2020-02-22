3d48e7de1e6d2af70f817c3eb7b9683360f569fd
AhmedHani <ahani.fcis@gmail.com>
2020-02-22 20:09:05 +0200

    adding setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='textstyle',
+    version='0.0.1',
+    packages=['tests', 'textstyle'],
+    url='https://github.com/AhmedHani/textstyle',
+    license='BSD 3-Clause License',
+    author='Ahmed H. Al-Ghidani',
+    author_email='ahani.fcis@gmail.com',
+    description='BSD 3-Clause License'
+)
